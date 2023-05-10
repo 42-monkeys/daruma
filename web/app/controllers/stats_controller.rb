08ee053e7eb4cafae5cc54b3a602540e53432535
baldarn <lorenzo@farnararo.it>
2023-05-10 11:17:55 +0200

    showing our costs

---
+++ web/app/controllers/stats_controller.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class StatsController < ApplicationController
+  skip_before_action :authenticate_user!
+
+  def index
+    @stats = {}
+    @stats[:total_spent] = Resolution.all.map(&:cost).sum # TODO: optimize :/
+  end
+end
--- web/config/routes.rb
+++ web/config/routes.rb
@@ -3,4 +3,6 @@
 Rails.application.routes.draw do
   resources :resolutions
   devise_for :users
+
+  get '/stats', to: 'stats#index'
 end
