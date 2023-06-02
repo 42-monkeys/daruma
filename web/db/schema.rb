5be90218ce8bc2d78efd3b9cb4783c965df75bd7
baldarn <lorenzo@farnararo.it>
2023-06-02 07:28:41 +0200

    updated schema

--- web/db/schema.rb
+++ web/db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,143 +10,142 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_230_531_145_209) do
-  create_table 'devices', force: :cascade do |t|
-    t.integer 'user_id'
-    t.string 'token'
-    t.string 'platform'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['user_id'], name: 'index_devices_on_user_id'
+ActiveRecord::Schema[7.0].define(version: 2023_05_31_145209) do
+  create_table "devices", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "token"
+    t.string "platform"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_devices_on_user_id"
   end
 
-  create_table 'jwt_denylist', force: :cascade do |t|
-    t.string 'jti', null: false
-    t.datetime 'exp', null: false
-    t.index ['jti'], name: 'index_jwt_denylist_on_jti'
+  create_table "jwt_denylist", force: :cascade do |t|
+    t.string "jti", null: false
+    t.datetime "exp", null: false
+    t.index ["jti"], name: "index_jwt_denylist_on_jti"
   end
 
-  create_table 'reminders', force: :cascade do |t|
-    t.text 'body'
-    t.integer 'resolution_id', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.boolean 'sent'
-    t.integer 'prompt_tokens'
-    t.integer 'completion_tokens'
-    t.index ['resolution_id'], name: 'index_reminders_on_resolution_id'
+  create_table "reminders", force: :cascade do |t|
+    t.text "body"
+    t.integer "resolution_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.boolean "sent"
+    t.integer "prompt_tokens"
+    t.integer "completion_tokens"
+    t.index ["resolution_id"], name: "index_reminders_on_resolution_id"
   end
 
-  create_table 'resolutions', force: :cascade do |t|
-    t.text 'body'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.integer 'user_id', null: false
-    t.date 'time_limit'
-    t.integer 'commitment'
-    t.integer 'temper'
-    t.integer 'offer'
-    t.boolean 'completed'
-    t.index ['user_id'], name: 'index_resolutions_on_user_id'
+  create_table "resolutions", force: :cascade do |t|
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.date "time_limit"
+    t.integer "commitment"
+    t.integer "temper"
+    t.integer "offer"
+    t.boolean "completed"
+    t.index ["user_id"], name: "index_resolutions_on_user_id"
   end
 
-  create_table 'rpush_apps', force: :cascade do |t|
-    t.string 'name', null: false
-    t.string 'environment'
-    t.text 'certificate'
-    t.string 'password'
-    t.integer 'connections', default: 1, null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.string 'type', null: false
-    t.string 'auth_key'
-    t.string 'client_id'
-    t.string 'client_secret'
-    t.string 'access_token'
-    t.datetime 'access_token_expiration'
-    t.text 'apn_key'
-    t.string 'apn_key_id'
-    t.string 'team_id'
-    t.string 'bundle_id'
-    t.boolean 'feedback_enabled', default: true
+  create_table "rpush_apps", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "environment"
+    t.text "certificate"
+    t.string "password"
+    t.integer "connections", default: 1, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "type", null: false
+    t.string "auth_key"
+    t.string "client_id"
+    t.string "client_secret"
+    t.string "access_token"
+    t.datetime "access_token_expiration"
+    t.text "apn_key"
+    t.string "apn_key_id"
+    t.string "team_id"
+    t.string "bundle_id"
+    t.boolean "feedback_enabled", default: true
   end
 
-  create_table 'rpush_feedback', force: :cascade do |t|
-    t.string 'device_token', limit: 64
-    t.datetime 'failed_at', precision: nil, null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.integer 'app_id'
-    t.index ['device_token'], name: 'index_rpush_feedback_on_device_token'
+  create_table "rpush_feedback", force: :cascade do |t|
+    t.string "device_token", limit: 64
+    t.datetime "failed_at", precision: nil, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "app_id"
+    t.index ["device_token"], name: "index_rpush_feedback_on_device_token"
   end
 
-  create_table 'rpush_notifications', force: :cascade do |t|
-    t.integer 'badge'
-    t.string 'device_token', limit: 64
-    t.string 'sound'
-    t.text 'alert'
-    t.text 'data'
-    t.integer 'expiry', default: 86_400
-    t.boolean 'delivered', default: false, null: false
-    t.datetime 'delivered_at', precision: nil
-    t.boolean 'failed', default: false, null: false
-    t.datetime 'failed_at', precision: nil
-    t.integer 'error_code'
-    t.text 'error_description'
-    t.datetime 'deliver_after', precision: nil
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.boolean 'alert_is_json', default: false, null: false
-    t.string 'type', null: false
-    t.string 'collapse_key'
-    t.boolean 'delay_while_idle', default: false, null: false
-    t.text 'registration_ids'
-    t.integer 'app_id', null: false
-    t.integer 'retries', default: 0
-    t.string 'uri'
-    t.datetime 'fail_after', precision: nil
-    t.boolean 'processing', default: false, null: false
-    t.integer 'priority'
-    t.text 'url_args'
-    t.string 'category'
-    t.boolean 'content_available', default: false, null: false
-    t.text 'notification'
-    t.boolean 'mutable_content', default: false, null: false
-    t.string 'external_device_id'
-    t.string 'thread_id'
-    t.boolean 'dry_run', default: false, null: false
-    t.boolean 'sound_is_json', default: false
-    t.index %w[delivered failed processing deliver_after created_at],
-            name: 'index_rpush_notifications_multi', where: 'NOT delivered AND NOT failed'
+  create_table "rpush_notifications", force: :cascade do |t|
+    t.integer "badge"
+    t.string "device_token", limit: 64
+    t.string "sound"
+    t.text "alert"
+    t.text "data"
+    t.integer "expiry", default: 86400
+    t.boolean "delivered", default: false, null: false
+    t.datetime "delivered_at", precision: nil
+    t.boolean "failed", default: false, null: false
+    t.datetime "failed_at", precision: nil
+    t.integer "error_code"
+    t.text "error_description"
+    t.datetime "deliver_after", precision: nil
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.boolean "alert_is_json", default: false, null: false
+    t.string "type", null: false
+    t.string "collapse_key"
+    t.boolean "delay_while_idle", default: false, null: false
+    t.text "registration_ids"
+    t.integer "app_id", null: false
+    t.integer "retries", default: 0
+    t.string "uri"
+    t.datetime "fail_after", precision: nil
+    t.boolean "processing", default: false, null: false
+    t.integer "priority"
+    t.text "url_args"
+    t.string "category"
+    t.boolean "content_available", default: false, null: false
+    t.text "notification"
+    t.boolean "mutable_content", default: false, null: false
+    t.string "external_device_id"
+    t.string "thread_id"
+    t.boolean "dry_run", default: false, null: false
+    t.boolean "sound_is_json", default: false
+    t.index ["delivered", "failed", "processing", "deliver_after", "created_at"], name: "index_rpush_notifications_multi", where: "NOT delivered AND NOT failed"
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'email', default: '', null: false
-    t.string 'encrypted_password', default: '', null: false
-    t.string 'reset_password_token'
-    t.datetime 'reset_password_sent_at'
-    t.datetime 'remember_created_at'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.string 'language'
-    t.string 'name'
-    t.integer 'role', default: 0
-    t.string 'time_zone'
-    t.string 'confirmation_token'
-    t.string 'unconfirmed_email'
-    t.datetime 'confirmed_at'
-    t.datetime 'confirmation_sent_at'
-    t.integer 'sign_in_count'
-    t.datetime 'current_sign_in_at'
-    t.datetime 'last_sign_in_at'
-    t.string 'current_sign_in_ip'
-    t.string 'last_sign_in_ip'
-    t.index ['confirmation_token'], name: 'index_users_on_confirmation_token', unique: true
-    t.index ['email'], name: 'index_users_on_email', unique: true
-    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "language"
+    t.string "name"
+    t.integer "role", default: 0
+    t.string "time_zone"
+    t.string "confirmation_token"
+    t.string "unconfirmed_email"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.integer "sign_in_count"
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key 'devices', 'users'
-  add_foreign_key 'reminders', 'resolutions'
-  add_foreign_key 'resolutions', 'users'
+  add_foreign_key "devices", "users"
+  add_foreign_key "reminders", "resolutions"
+  add_foreign_key "resolutions", "users"
 end
